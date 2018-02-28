03ddb604930bf5c5517d8584cb5c77a61f43a320
VickyMutai <vicky.mutai96@gmail.com>
2018-02-28 15:44:33 +0300

    set up methods on views

--- galleria/photos/views.py
+++ galleria/photos/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return HttpResponse('Photogallery')
+
+def search_results(request):
+    pass
+
+def image(request):
+    pass
+
+def filter_location(request):
+    pass
