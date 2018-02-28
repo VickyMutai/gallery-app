2b777f97c60ef1790f42e929c0f744cc864d0421
VickyMutai <vicky.mutai96@gmail.com>
2018-02-28 15:47:38 +0300

    adding index page

--- galleria/photos/views.py
+++ galleria/photos/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Photogallery')
-
+    return render(request,'index.html')
 def search_results(request):
     pass
 
