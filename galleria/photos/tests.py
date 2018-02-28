d123bd76c8fa87d36b7804b5cc74ed2fb17bb387
VickyMutai <vicky.mutai96@gmail.com>
2018-02-28 16:57:32 +0300

    write test for checking instance

--- galleria/photos/tests.py
+++ galleria/photos/tests.py
@@ -8,10 +8,19 @@ class CategoryTestClass(TestCase):
     def setUp(self):
         self.nature = Category(category_name="nature")
 
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nature,Category))
+
 class LocationTestClass(TestCase):
     #set up method
     def setUp(self):
         self.tsavo = Location(place="Tsavo")
+
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.tsavo,Location))
+
 
 class ImageTestClass(TestCase):
     def setUp(self):
@@ -22,6 +31,10 @@ class ImageTestClass(TestCase):
         #creating new category and saving it
         self.nature = Category(category_name="nature")
         self.nature.save()
+
+        #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.elephant,Image))
 
         #creating new location and saving it
         self.tsavo = Location(place="Tsavo")
