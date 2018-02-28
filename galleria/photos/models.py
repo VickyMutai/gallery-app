d27f1065762edb9649304c9435e93d16c6cea126
VickyMutai <vicky.mutai96@gmail.com>
2018-02-28 16:43:03 +0300

    download pillow and migrate image model

--- galleria/photos/models.py
+++ galleria/photos/models.py
@@ -12,3 +12,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    image_url = models.ImageField(upload_to = 'categories/',null=True,blank=True)
+    location = models.ManyToManyField(Location)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
