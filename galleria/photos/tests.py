4f560a3a69b8d46084d2a9c9171ca83ce42ed427
VickyMutai <vicky.mutai96@gmail.com>
2018-02-28 16:55:04 +0300

    setting up test cases for models

--- galleria/photos/tests.py
+++ galleria/photos/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.nature = Category(category_name="nature")
+
+class LocationTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.tsavo = Location(place="Tsavo")
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+
+        self.elephant = Image(name="Elephant",description="It is super big")
+        self.elephant.save()
+
+        #creating new category and saving it
+        self.nature = Category(category_name="nature")
+        self.nature.save()
+
+        #creating new location and saving it
+        self.tsavo = Location(place="Tsavo")
+        self.tsavo.save()
+
+        self.elephant.location.add(self.tsavo)
+        self.elephant.category.add(self.nature)
+
